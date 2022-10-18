aecfaa5218b125209e25b9ccae0929d0a1a66140
Jaiganesh <jaiganesh.bthoughtworks.com@Jaiganeshs-MacBook-Pro.local>
2022-10-18 15:51:43 +0530

    initial commit

---
+++ src/main/java/com/tweats/tweats/TweatsApplication.java
@@ -0,0 +1,13 @@
+package com.tweats.tweats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweatsApplication.class, args);
+	}
+
+}
