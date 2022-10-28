a1af7a74d7ead69c9d932ebb57b0911c4ef8a12f
Latha Sree Kunta Rangayyagari <lathasree.k@Lathas-MacBook-Pro.local>
2022-10-28 12:56:45 +0530

    [Latha | Preeti |Jaiganesh] Add. get image service

--- src/main/java/com/tweats/tweats/image/ImageService.java
+++ src/main/java/com/tweats/tweats/image/ImageService.java
@@ -1,5 +1,6 @@
 package com.tweats.tweats.image;
 
+import com.tweats.tweats.exceptions.ImageNotFoundException;
 import com.tweats.tweats.exceptions.NotAnImageException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Service
 public class ImageService {
@@ -18,12 +20,21 @@ public class ImageService {
         this.imageRepository = imageRepository;
     }
 
-    public void save(MultipartFile file) throws IOException, NotAnImageException {
+    public Image save(MultipartFile file) throws IOException, NotAnImageException {
         if (file.getContentType() == MediaType.IMAGE_PNG_VALUE || file.getContentType() == MediaType.IMAGE_JPEG_VALUE) {
             Image image = new Image(file.getName(), file.getContentType(), file.getBytes(), file.getSize());
             imageRepository.save(image);
-            return;
+            return image;
         }
         throw new NotAnImageException();
     }
+
+    public Image getImage(String imageId) throws ImageNotFoundException {
+        return imageRepository.findById(imageId).orElseThrow(() ->  new ImageNotFoundException());
+    }
+
+
+
+//    public MultipartFile fetch(org.springframework.mock.web.MockMultipartFile mockMultipartFile) {
+//    }
 }
--- src/test/java/com/tweats/tweats/image/ImageServiceTest.java
+++ src/test/java/com/tweats/tweats/image/ImageServiceTest.java
@@ -1,17 +1,21 @@
 package com.tweats.tweats.image;
 
+import com.tweats.tweats.exceptions.ImageNotFoundException;
 import com.tweats.tweats.exceptions.NotAnImageException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
 
 public class ImageServiceTest {
     private ImageRepository imageRepository;
@@ -20,23 +24,38 @@ public class ImageServiceTest {
     @BeforeEach
     public void setup() {
         imageRepository = mock(ImageRepository.class);
-
-
     }
 
     @Test
     void shouldBeAbleToSaveImageWhenValidDetailsAreProvided() throws IOException, NotAnImageException {
         MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "image.png", MediaType.IMAGE_JPEG_VALUE, "hello".getBytes());
         ImageService imageService = new ImageService(imageRepository);
+
         imageService.save(mockMultipartFile);
+
         verify(imageRepository).save(any());
 
     }
 
     @Test
-    void shouldNotBeAbleToSaveImageWhenGivenFileIsNotImage() throws IOException, NotAnImageException {
+    void shouldNotBeAbleToSaveImageWhenGivenFileIsNotImage() {
         MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "image.png", MediaType.APPLICATION_FORM_URLENCODED_VALUE, "hello".getBytes());
         ImageService imageService = new ImageService(imageRepository);
+
         assertThrows(NotAnImageException.class, () -> imageService.save(mockMultipartFile));
+    }
+
+    @Test
+    void shouldBeAbleToFetchImageWhenValidDetailsAreProvided() throws ImageNotFoundException {
+        String imageId = "Image@123";
+        Image image = new Image();
+        when(imageRepository.findById(imageId))
+                .thenReturn(Optional.of(image));
+        ImageService imageService = new ImageService(imageRepository);
+
+        Image fetchedImage = imageService.getImage(imageId);
+
+        assertThat(image, is(fetchedImage));
+
     }
 }
