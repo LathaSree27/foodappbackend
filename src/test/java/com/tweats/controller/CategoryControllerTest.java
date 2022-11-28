2ff4d7f85308eedc1a66b0b9f43e4304ed2ecd5f
Latha Sree Kunta Rangayyagari <lathasree.k@Lathas-MacBook-Pro.local>
2022-11-28 15:10:02 +0530

    [Latha | Amrutha] Add. controller test for saving category

--- src/test/java/com/tweats/controller/CategoryControllerTest.java
+++ src/test/java/com/tweats/controller/CategoryControllerTest.java
@@ -1,14 +1,16 @@
 package com.tweats.controller;
 
 import com.tweats.controller.response.CategoryResponse;
-import com.tweats.exceptions.NoCategoryFoundException;
+import com.tweats.exceptions.*;
 import com.tweats.service.CategoryService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +28,17 @@ public class CategoryControllerTest {
     @InjectMocks
     private CategoryController categoryController;
 
+    @Test
+    void shouldBeAbleToSaveCategoryWhenNameFileAndUseremailIsGiven() throws UserNotFoundException, ImageSizeExceededException, IOException, NotAVendorException, CategoryAlreadyAssignedException, NotAnImageException {
+        MockMultipartFile categoryImageFile = new MockMultipartFile("image.png", "Hello".getBytes());
+        String name = "juice";
+        String user_email = "abc@gmail.com";
 
+        categoryController.save(categoryImageFile,name,user_email);
+
+        verify(categoryService).save(name,categoryImageFile,user_email);
+
+    }
     @Test
     void shouldBeAbleToReturnAllCategoriesWhenUserLoggedIn() throws NoCategoryFoundException {
         List<CategoryResponse> expectedCategoryResponses = new ArrayList<>();
@@ -35,4 +47,6 @@ public class CategoryControllerTest {
         verify(categoryService).getAllCategories();
         assertThat(actualCategoryResponses, is(expectedCategoryResponses));
     }
+
+
 }
